2b797b35e944a19907e9759dda3d88d26f514275
Ayaan Rathod <ayaanrathod58@gmail.com>
2025-03-19 04:13:30 -0400

    Initial files uploaded

---
+++ .dist/JS/script.js
@@ -0,0 +1,4 @@
+// Check that the JavaScript is working by logging a message to the console.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to Group 16 Ideal Site!');
+  });
